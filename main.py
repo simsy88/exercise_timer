3072c2437e9c8482a5f3829172961fa287b5cca9
simsy88 <andrew_sims1@hotmail.com>
2020-07-22 19:23:10 +0100

    Improve formatting

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import tkinter as tk
+from tkinter import font
+
+from programme_directory import test_programme, upper_programme
 
 
 class Programme:
     def __init__(self, parent, stations, n_circuits):
-        # labels
+        # base stuff
         self.parent = parent
+        self.huge_font = font.Font(self.parent, size=120)
+        self.big_font = font.Font(self.parent, size=80)
+        self.small_font = font.Font(self.parent, size=40)
+        self.programme = stations * n_circuits
+        # labels
         self.title_label = tk.Label(parent)
-        self.title_label.pack()
+        self.title_label.config(font=self.big_font)
+        self.title_label.pack(fill=tk.BOTH, expand=1)
         self.details_label = tk.Label(parent)
-        self.details_label.pack()
+        self.details_label.config(font=self.small_font)
+        self.details_label.pack(fill=tk.BOTH, expand=1)
         self.countdown_label = tk.Label(parent)
-        self.countdown_label.pack()
-        self.programme = stations*n_circuits
+        self.countdown_label.config(font=self.huge_font)
+        self.countdown_label.pack(fill=tk.BOTH, expand=1)
         # start the timer
         self.parent.after(0, self.display_station, 0)
 
@@ -24,26 +34,26 @@ class Programme:
             root.after(0, function, *args)
 
     def display_station(self, n):
-        self.title_label.configure(text=self.programme[n]['name'])
-        self.details_label.configure(text=self.programme[n]['description'])
+        self.title_label.configure(text=self.programme[n]['name'], background='orange')
+        self.details_label.configure(text=self.programme[n]['description'], background='orange')
+        self.countdown_label.configure(background='orange')
         self.parent.after(0, self.countdown, self.programme[n]['time'], self.display_rest, n)
 
     def display_rest(self, n):
         if n == len(self.programme)-1:
-            self.title_label.configure(text='Well Done!')
-            self.details_label.configure(text='Your workout is complete')
-            self.countdown_label.configure(text='')
-            self.parent.after(5000, self.parent.destroy)
+            self.title_label.configure(text='Well Done!', background='black', foreground='white')
+            self.details_label.configure(text='Your workout is complete', background='white', foreground='black')
+            self.countdown_label.configure(text='', background='black', foreground='white')
+            self.parent.after(3000, self.parent.destroy)
         else:
-            self.title_label.configure(text='Rest')
-            self.details_label.configure(text=f'Next station: {self.programme[n + 1]["name"]}')
+            self.title_label.configure(text='Rest', background='lightblue')
+            self.details_label.configure(text=f'Next station: {self.programme[n + 1]["name"]}', background='lightblue')
+            self.countdown_label.configure(background='lightblue')
             self.parent.after(0, self.countdown, self.programme[n]['rest_time'], self.display_station, n+1)
-
-
-test_programme = [{'name': 'test1', 'description': 'testA', 'time': 3, 'rest_time': 2},
-                  {'name': 'test2', 'description': 'testB', 'time': 5, 'rest_time': 10}]
 
 if __name__ == "__main__":
     root = tk.Tk()
+    root.state('zoomed')
+#    p = Programme(root, upper_programme, 3)
     p = Programme(root, test_programme, 2)
     root.mainloop()
---
+++ programme_directory.py
@@ -0,0 +1,15 @@
+
+test_programme = [{'name': 'test1', 'description': 'testA', 'time': 3, 'rest_time': 2},
+                  {'name': 'test2', 'description': 'testB', 'time': 5, 'rest_time': 10}]
+
+upper_programme = [
+    {'name': 'Squat with shoulder press', 'description': 'Squat, arms out parallel to body, forearms up, palms flat facing forward. Occellate up and down at shoulder, keeping forearms vertical', 'time': 30, 'rest_time': 15},
+    {'name': 'Squat with shoulder rotations', 'description': 'Squat, arms out parallel to body, forearms up, palms flat facing forward. Rotate at shoulder to bring forearms flat, then back vertical again', 'time': 30, 'rest_time': 15},
+    {'name': 'Squat with rows', 'description': 'Squat, arms out parallel to body, forearms flat, palms flat facing down. Occellate at shoulder to arms straight out in front, keeping forearms parallel to each other', 'time': 30, 'rest_time': 15},
+    {'name': 'Alternating single leg walk out', 'description': 'Balance on one leg. Bend down and put hands on the floor. With foot still raised, “walk” out with hands until grounded leg is straight, “walk” back again, and stand up. Repeat with other leg', 'time': 30, 'rest_time': 15},
+    {'name': 'Scapular pushups', 'description': 'In a press-up position, move up and down with shoulder movement only. Keep arms locked straight', 'time': 30, 'rest_time': 15},
+    {'name': 'Push ups', 'description': '', 'time': 30, 'rest_time': 15},
+    {'name': 'Plank to elbow dips (left)', 'description': 'In a press-up position, lower such that left forearm is on the ground, orthogonal to body and in line with right hand, then raise back into press-up position', 'time': 30, 'rest_time': 15},
+    {'name': 'Plank to elbow dips (right)', 'description': 'In a press-up position, lower such that right forearm is on the ground, orthogonal to body and in line with left hand, then raise back into press-up position', 'time': 30, 'rest_time': 15},
+    {'name': 'Tricep presses', 'description': 'Crouched down, knees on floor, forearms on floor pointing forward, upper arms vertical. Raise bum up until arms and legs are straight. At top, arms should be vertical. Then back to original crouch', 'time': 30, 'rest_time': 15},
+]
